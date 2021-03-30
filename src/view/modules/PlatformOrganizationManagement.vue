01feffedd16b1a07707f6c91c1d0ec0aa6fa91b2
sunbinhub <349608572@qq.com>
2021-03-30 17:01:12 +0800

    平台机构管理初步完成

--- src/view/modules/PlatformOrganizationManagement.vue
+++ src/view/modules/PlatformOrganizationManagement.vue
@@ -52,7 +52,11 @@
                   <el-option label="在用" value="0"></el-option>
                   <el-option label="停用" value="1"></el-option>
                 </el-select>
-                <SearchInput @searchValue="searchOrganization"></SearchInput>
+
+                <SearchInput
+                  @searchValue="searchOrganization"
+                  placeholder="请输入组织名称/ID"
+                ></SearchInput>
               </el-col>
               <el-col :span="6">
                 <el-button type="info" size="mini">查询</el-button>
@@ -146,117 +150,13 @@
           </el-main>
           <!-- 右侧表格结束 -->
           <!-- 弹窗开始 -->
-          <el-dialog
-            title="新增组织机构"
-            :visible.sync="dialogFormVisible"
-            class="organization-dialog"
-          >
-            <el-form
-              :model="organizationForm"
-              :rules="organizationRules"
-              ref="organizationRules"
-            >
-              <el-form-item
-                label="组织名称"
-                :label-width="formLabelWidth"
-                prop="organizationName"
-                size="mini"
-              >
-                <el-input
-                  v-model="organizationForm.organizationName"
-                  placeholder="请输入内容"
-                  size="mini"
-                ></el-input>
-              </el-form-item>
-              <el-form-item
-                label="组织全称"
-                :label-width="formLabelWidth"
-                size="mini"
-              >
-                <el-input
-                  v-model="organizationForm.organizationFullname"
-                  placeholder="请输入内容"
-                  size="mini"
-                ></el-input>
-              </el-form-item>
-              <el-form-item
-                label="组织代码"
-                :label-width="formLabelWidth"
-                prop="organizationCode"
-                size="mini"
-              >
-                <el-input
-                  v-model="organizationForm.organizationCode"
-                  placeholder="请输入内容"
-                  size="mini"
-                ></el-input>
-              </el-form-item>
-              <el-form-item
-                label="选择类型"
-                :label-width="formLabelWidth"
-                prop="dialogOrganizationValue"
-                size="mini"
-              >
-                <el-select
-                  v-model="organizationForm.dialogOrganizationValue"
-                  placeholder="请选择活动区域"
-                  size="mini"
-                >
-                  <el-option
-                    v-for="item in dialogOrganizationOptions"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value"
-                    size="mini"
-                  >
-                  </el-option>
-                </el-select>
-              </el-form-item>
-              <el-form-item
-                label="上级机构"
-                :label-width="formLabelWidth"
-                prop="dialogParentOrganizationValue"
-                size="mini"
-              >
-                <el-select
-                  v-model="organizationForm.dialogParentOrganizationValue"
-                  placeholder="请选择活动区域"
-                  size="mini"
-                >
-                  <el-option
-                    v-for="item in dialogParentOrganizationOptions"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value"
-                    size="mini"
-                  >
-                  </el-option>
-                </el-select>
-              </el-form-item>
-              <el-form-item
-                label="排序号"
-                :label-width="formLabelWidth"
-                size="mini"
-              >
-                <el-input
-                  v-model="organizationForm.orderID"
-                  placeholder="请输入内容"
-                  size="mini"
-                ></el-input>
-              </el-form-item>
-            </el-form>
-            <div slot="footer" class="dialog-footer">
-              <el-button @click="closeForm('organizationRules')" size="mini">
-                取消
-              </el-button>
-              <el-button
-                type="primary"
-                @click="submitDialogForm('organizationRules')"
-                size="mini"
-              >
-                保存
-              </el-button>
-            </div>
+          <el-dialog :visible.sync="dialogFormVisible">
+            <AddOrganization
+              @close="dialogFormVisible = false"
+              @refresh="refreshTable"
+              :organizationForm="changeOrganization"
+              :parentId="userInfo.id"
+            ></AddOrganization>
           </el-dialog>
           <!-- 弹窗结束 -->
         </el-container>
@@ -267,12 +167,19 @@
   </el-container>
 </template>
 <script>
+import { mapState, mapGetters } from "vuex";
 import SearchInput from "@/components/common-components/SearchInput";
 import Pagination from "@/components/common-components/Pagination";
-import { mapState, mapGetters } from "vuex";
+import AddOrganization from "@/view/modules/module-component/pom/AddOrganization";
 
 export default {
-  components: { SearchInput, Pagination, mapState, mapGetters },
+  components: {
+    SearchInput,
+    Pagination,
+    mapState,
+    mapGetters,
+    AddOrganization
+  },
   data() {
     return {
       //树形控件
@@ -295,79 +202,10 @@ export default {
       multipleSelection: [],
       tableHeight: "", //表格高度
       dialogFormVisible: false, //弹窗是否显示
-      formLabelWidth: "120px", //弹窗内部左侧的距离
-      //弹窗内组织选项
-      dialogOrganizationOptions: [
-        {
-          value: "选项1",
-          label: "黄金糕"
-        },
-        {
-          value: "选项2",
-          label: "双皮奶"
-        },
-        {
-          value: "选项3",
-          label: "蚵仔煎"
-        },
-        {
-          value: "选项4",
-          label: "龙须面"
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭"
-        }
-      ],
-      //弹窗内上级机构选项
-      dialogParentOrganizationOptions: [
-        {
-          value: "选项1",
-          label: "黄金糕"
-        },
-        {
-          value: "选项2",
-          label: "双皮奶"
-        },
-        {
-          value: "选项3",
-          label: "蚵仔煎"
-        },
-        {
-          value: "选项4",
-          label: "龙须面"
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭"
-        }
-      ],
-      organizationForm: {
-        dialogOrganizationValue: "", //弹窗内选择类型选项值
-        dialogParentOrganizationValue: "", //弹窗内上级机构选项值
-        organizationName: "", //组织名称
-        orderID: "", //排序号
-        organizationFullname: "", //组织全称
-        organizationCode: "" //组织代码
-      },
-      organizationRules: {
-        organizationName: [
-          { required: true, message: "请输入组织名称", trigger: "blur" },
-          { min: 1, max: 6, message: "长度在 1 到 6 个字符", trigger: "blur" }
-        ],
-        organizationCode: [
-          { required: true, message: "请输入组织代码", trigger: "blur" },
-          { min: 1, max: 6, message: "长度在 1 到 6 个字符", trigger: "blur" }
-        ],
-        dialogOrganizationValue: [
-          { required: true, message: "请输入选择类型", trigger: "change" }
-        ],
-        dialogParentOrganizationValue: [
-          { required: true, message: "请输入上级机构", trigger: "change" }
-        ]
-      },
       totalCount: null, //表格数据总条数
-      nodeClickId: null
+      nodeClickId: null, //组织机构id
+      changeOrganization: {}, //修改组织的信息
+      parentId: "" //当前用户id
     };
   }, // 生命周期 - 创建完成（可以访问当前this实例）
   created() {
@@ -381,9 +219,6 @@ export default {
   watch: {
     filterText(val) {
       this.$refs.tree.filter(val);
-    },
-    organizationStatus(val) {
-      debugger;
     }
   },
   methods: {
@@ -434,52 +269,69 @@ export default {
       // 获取浏览器高度，减去顶部导航栏的值70（可动态获取）
       this.platformAsideHeight.height = window.innerHeight - 172 + "px";
     },
+    //搜索功能
     searchOrganization(value) {
-      //这里继续开始写。。。。。。。。。搜索功能
-      console.log(value);
-      this.nodeClickId;
-      this.organizationStatus;
-      let aaa = {
-        parentId: "",
-        disableFlag: "",
-        condition: "",
-        limit: 10,
-        page: 1
+      let searchParams = {
+        parentId: this.nodeClickId, //机构id
+        disableFlag: this.organizationStatus, //状态
+        condition: value, //搜索值
+        limit: 10, //每页10条
+        page: 1 //第一页
       };
-      debugger;
+      this.getlist(searchParams);
     },
     getTableHeight() {
       // 获取浏览器高度，减去顶部导航栏的值70（可动态获取）
       this.tableHeight = window.innerHeight - 267 + "px";
     },
-    //提交弹窗表单
-    submitDialogForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    },
-    //重置弹窗表格并关闭
-    closeForm(formName) {
-      this.$refs[formName].resetFields();
-      this.dialogFormVisible = false;
-    },
     //表格修改事件
     changeClick(row) {
+      row.type = row.type.toString(); //解决select option不能对应显示的问题
+      this.changeOrganization = row;
       this.dialogFormVisible = true;
-      console.log(row);
-      //...这一行的数据赋值
     },
     //表格删除事件
     removeClick(row) {
-      //...删除接口
+      debugger;
+      this.axios({
+        method: "post",
+        url: "http://192.168.0.40:9900/uc/sys/organization/delete/" + row.id,
+        headers: {
+          authorization: this.tokenValue
+        }
+      }).then(res => {
+        //获取表格数据，默认第一页 10条
+        if (res.data && res.data.code === 0) {
+          this.$message.success("删除组织机构成功！");
+          this.getlist({ limit: 10, page: 1 }); //刷新表格数据
+        } else {
+          this.$message.error(res.data.msg);
+        }
+      });
     },
     //表格停用事件
-    disableClick(row) {},
+    disableClick(row) {
+      debugger;
+      this.axios({
+        method: "post",
+        url:
+          "http://192.168.0.40:9900/uc/sys/organization/switch/" +
+          row.id +
+          "/" +
+          1,
+        headers: {
+          authorization: this.tokenValue
+        }
+      }).then(res => {
+        //获取表格数据，默认第一页 10条
+        if (res.data && res.data.code === 0) {
+          this.$message.success("停用组织机构成功！");
+          this.getlist({ limit: 10, page: 1 }); //刷新表格数据
+        } else {
+          this.$message.error(res.data.msg);
+        }
+      });
+    },
     //表格点击事件
     cellClick(row, column, event, cell) {
       this.axios
@@ -524,6 +376,10 @@ export default {
     nodeClick(row) {
       //树形控件点击事件
       this.nodeClickId = row.id;
+    },
+    //新增组织成功刷新表格
+    refreshTable(val) {
+      this.getlist(val);
     }
   },
   computed: {
@@ -550,8 +406,5 @@ export default {
 
 .fr {
   float: right;
-}
-.organization-dialog {
-  margin-top: -85px;
 }
 </style>
